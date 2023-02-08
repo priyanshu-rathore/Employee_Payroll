a8bd6faaaf61d764e15e50815bc9b7e0d5a4b51f
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-02-08 07:46:24 +0530

    Create Table

--- EmployeePayrollSQLFile.sql
+++ EmployeePayrollSQLFile.sql
@@ -1,2 +1,9 @@
 create database payroll_service;
 use payroll_service;
+
+create table employee_payroll
+(
+EmployeeID int identity(1,1) primary key,
+EmployeeName varchar(255),
+Salary float,
+StartDate Date);
